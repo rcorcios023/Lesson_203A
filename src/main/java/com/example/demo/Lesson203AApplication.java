ac79f3aa65e34941e6e222149ac0ce17c1574db2
rcorcios023 <56006220+rcorcios023@users.noreply.github.com>
2019-10-28 22:08:40 -0400

    lesson_203A

---
+++ src/main/java/com/example/demo/Lesson203AApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson203AApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson203AApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demo/Patient.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Patient{
+    private String name;
+    private String lastName;
+    private int ssn;
+    private int dob;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(int ssn) {
+        this.ssn = ssn;
+    }
+
+    public int getDob() {
+        return dob;
+    }
+
+    public void setDob(int dob) {
+        this.dob = dob;
+    }
+}
